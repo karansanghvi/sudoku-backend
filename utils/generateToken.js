f81f1930a9a22a83761b486140a700902210fc5e
Karan Sanghvi <karansanghvi1303@gmail.com>
2025-12-06 11:36:27 -0800

    fixed token issue

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,12 +1,17 @@
 const generateToken = (res, userId) => {
-    const token = userId.toString(); 
-    res.cookie('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-        maxAge: 30 * 24 * 60 * 60 * 1000
-    });
-    return token;
+  const token = userId.toString(); 
+  
+  const isProduction = process.env.NODE_ENV === 'production';
+  
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: isProduction, 
+    sameSite: isProduction ? 'none' : 'lax', 
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    domain: isProduction ? undefined : 'localhost' 
+  });
+  
+  return token;
 };
 
 module.exports = generateToken;
