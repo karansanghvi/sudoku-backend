81ad89301b8ac86ac2b111ebb0b78259db76d1d7
Karan Sanghvi <karansanghvi1303@gmail.com>
2025-12-06 12:05:01 -0800

    fixed token as both are on render

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,13 +1,11 @@
 const generateToken = (res, userId) => {
   const token = userId.toString(); 
   
-  const isProduction = process.env.NODE_ENV === 'production';
-  
   res.cookie('token', token, {
     httpOnly: true,
-    secure: isProduction, 
-    sameSite: isProduction ? 'none' : 'lax', 
-    maxAge: 30 * 24 * 60 * 60 * 1000, 
+    secure: true, 
+    sameSite: 'none', 
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
   
   return token;
