2b3f61e087735a1ee85389ba50a3e61a6525939a
Karan Sanghvi <karansanghvi1303@gmail.com>
2025-12-06 11:44:58 -0800

    fix cookie domain and sameSite for production

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,8 +7,7 @@ const generateToken = (res, userId) => {
     httpOnly: true,
     secure: isProduction, 
     sameSite: isProduction ? 'none' : 'lax', 
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    domain: isProduction ? undefined : 'localhost' 
+    maxAge: 30 * 24 * 60 * 60 * 1000, 
   });
   
   return token;
